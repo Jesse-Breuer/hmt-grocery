52b1a05b37de2c1cc831dda14493479c62461f75
Jesse Breuer <Jesse.Breuer@gmail.com>
2011-10-22 05:47:36 -0400

    first version of stores partial

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ActiveRecord::Base
   belongs_to :user
   belongs_to :item
+  accepts_nested_attributes_for :item
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,4 +1,5 @@
 class Store < ActiveRecord::Base
   belongs_to :item
   belongs_to :detail
+  accepts_nested_attributes_for :detail
 end
