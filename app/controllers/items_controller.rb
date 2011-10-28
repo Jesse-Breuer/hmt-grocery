b0391ff79696142c4e6533ff5f8c6004fd14374d
Jesse Breuer <Jesse.Breuer@gmail.com>
2011-10-27 22:07:11 -0400

    nested with errors

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,7 @@ class ItemsController < ApplicationController
   # GET /items/new.json
   def new
     @item = Item.new
+    @item.stores.build.build_detail
 
     respond_to do |format|
       format.html # new.html.erb
